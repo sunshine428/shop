ef8896fabff83632e1c278a67bebd6e76199cf85
sunshine428 <2484282331@qq.com>
2019-10-15 16:04:28 +0800

    aa

--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -17,9 +17,10 @@ class EventController extends Controller
         file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
         $xml_obj=simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
         $xml_arr=(array)$xml_obj;
-        $msg="沙雕！！";
 //        dd($xml_arr);
-        echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
-
+        if($xml_arr['Content']=='222'){
+            $msg="沙雕！！";
+            echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
+        }
     }
 }
